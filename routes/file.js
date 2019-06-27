3b2f14c3106e1158f494acd56e583354dd976bac
yiding.wan <yiding.wan@efastserv.com>
2019-06-28 00:20:14 +0800

    [feature] add upload demo

--- routes/file.js
+++ routes/file.js
@@ -1,10 +1,23 @@
+const fs = require('fs')
 const router = require('koa-router')()
+const koaBody = require('koa-body')
 
 router.prefix('/file')
 
-router.post('/upload', function(ctx, next) {
-  console.log(ctx.response.files)
-  ctx.body = 'upload'
+router.post('/upload', koaBody({multipart: true}), async function(ctx) {
+  const { name } = ctx.request.body
+  const file = ctx.request.files.file
+  const reader = fs.createReadStream(file.path)
+
+  return new Promise((resolve, reject) => {
+    fs.mkdir('./upload-temp', function (err) {
+      const upStream = fs.createWriteStream(`./upload-temp/${Math.random()}_${file.name}`)
+      reader.pipe(upStream)
+      resolve(() => {
+        ctx.body = `${name} upload success`
+      })
+    })
+  })
 })
 
 module.exports = router
