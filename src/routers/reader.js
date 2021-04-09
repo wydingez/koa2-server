095260e4d9950f9cc202699411cad066a017ab1b
wydingez <981342796@qq.com>
2021-04-09 16:26:01 +0800

    file update

--- src/routers/reader.js
+++ src/routers/reader.js
@@ -4,6 +4,9 @@ const path = require('path')
 const fse = require('fs-extra')
 const uuid = require('uuid')
 
+const send = require('koa-send');
+const archiver = require('archiver');
+
 const PUBLIC_PATH = path.resolve(__dirname, '../../public/file')
 
 router.prefix('/reader')
@@ -27,6 +30,7 @@ router.get('/listDoc', (ctx) => {
 
             r({
               name: file,
+              type: file.substring(file.lastIndexOf('.')),
               size: (fileInfo.size / 1024 / 1024).toFixed(2) + 'MB'
             })
           })
@@ -36,7 +40,7 @@ router.get('/listDoc', (ctx) => {
       Promise.all(promiseList).then((result) => {
         ctx.body = {
           code: 200,
-          data: result,
+          result: result,
           msg: '查询成功'
         }
   
@@ -47,9 +51,10 @@ router.get('/listDoc', (ctx) => {
 })
 
 // 读取文件信息
-router.post('/readDoc', (ctx) => {
+router.get('/readDoc/:name', (ctx) => {
+  const name = ctx.params.name
   return new Promise((resolve, reject) => {
-    textract.fromFileWithPath(PUBLIC_PATH + '/YD-C070100 顾客反馈处理程序.doc', (err, text) => {
+    textract.fromFileWithPath(`${PUBLIC_PATH}/${name}`, (err, text) => {
       if (err) {
         ctx.throw(500, err)
         console.log(err)
@@ -57,7 +62,7 @@ router.post('/readDoc', (ctx) => {
   
       ctx.body = {
         code: 200,
-        data: text,
+        result: text,
         msg: '解析成功'
       }
 
@@ -68,8 +73,13 @@ router.post('/readDoc', (ctx) => {
 
 // 上传文件
 router.post('/uploadDoc', (ctx) => {
-  const files = ctx.request.files.file
+  let files = ctx.request.files.file
   const confirmUnique = false
+
+  if (!Array.isArray(files)) {
+    files = [files]
+  }
+
   for (let file of files) {
     const reader = fse.createReadStream(file.path)
     
@@ -119,6 +129,54 @@ router.delete('/delDoc/:name', (ctx) => {
       r()
     })
   })
+})
+
+// 删除所有文件
+router.delete('/delAllDoc', (ctx) => {
+  return new Promise((r, j) => {
+    const fPath = `${PUBLIC_PATH}/`
+
+    fse.access(fPath, (err) => {
+      if (err) {
+        ctx.throw(500, '目录不存在')
+      } else {
+        fse.unlink(fPath, (err) => {
+          if (err) {
+            console.log(err)
+          }
+        })
+
+        ctx.body = {
+          code: 200,
+          msg: '删除成功'
+        }
+      }
+
+      r()
+    })
+  })
+})
+
+// 下载文件
+router.get('/download/:name', async (ctx) => {
+  const name = ctx.params.name
+  const path = `public/file/${name}`
+  ctx.attachment(path)
+  await send(ctx, path);
+})
+
+// 下载所有文件
+router.get('/downloadAll', async (ctx) => {
+  const zipName = 'all.zip'
+  const zipStream = fse.createWriteStream(zipName)
+  const zip = archiver('zip')
+  zip.pipe(zipStream)
+  // 添加整个文件夹到压缩包
+  zip.directory('public/file/', false)
+  await zip.finalize()
+
+  ctx.attachment(zipName)
+  await send(ctx, zipName)
 })
 
 module.exports = router
